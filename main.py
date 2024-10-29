e31748900335005dee7566e75138a7d0be736350
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 <vincenzo.amendola141@gmail.com>
2024-10-29 17:34:16 -0300

    feat: added basemodel config plus exceptions

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI, status, Response
 import requests
 from logger import AppLogger
+from pydantic import BaseModel
+from typing import Union
 
 logger = AppLogger().get_logger()
 
 app = FastAPI()
 
+class Prompt(BaseModel):
+    prompt: Union[str, int] = None
+
 @app.get('/')
 async def root():
     return {'message': 'Welcome to the FastAPI application!'}
 
-@app.get("/ask")
-def ask(prompt: str):
+@app.get("/ask", response_model= Prompt, status_code=status.HTTP_201_CREATED) 
+def ask(prompt: str) -> Prompt:
     logger.info(f"This provides an unleashed model with the prompt: {prompt}")
 
     try:
@@ -21,7 +26,7 @@ def ask(prompt: str):
             'http://127.0.0.1:11434/api/generate',
             json={"model": "llama3.2:1b", "prompt": prompt, "stream": False},
             headers=headers,
-            timeout=10  
+            timeout=20
             )
         res.raise_for_status()
     except requests.exceptions.RequestException as e:
