b4477b7bc793ff4245a41d3363f7828e4d284178
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 <vincenzo.amendola141@gmail.com>
2024-10-29 17:17:10 -0300

    initial committ

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a base image
+FROM python:3.10-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the requirements file to the working directory
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+# Expose the port that the app runs on
+EXPOSE 8000
+
+# Command to run the application using uvicorn
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--lifespan=on", "--use-colors", "--loop", "uvloop", "--http", "httptools", "--reload", "--log-level", "debug"]
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, status, Response
+import requests
+from logger import AppLogger
+
+logger = AppLogger().get_logger()
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message': 'Welcome to the FastAPI application!'}
+
+@app.get("/ask")
+def ask(prompt: str):
+    logger.info(f"This provides an unleashed model with the prompt: {prompt}")
+
+    try:
+
+        headers = {"Content-Type": "application/json"}
+        res = requests.post(
+            'http://127.0.0.1:11434/api/generate',
+            json={"model": "llama3.2:1b", "prompt": prompt},
+            headers=headers,
+            timeout=10  
+            )
+        res.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        
+        logger.error(f"Error while making a request to the model API: {e}")
+        return Response(content=f"Error occurred: {e}", status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(content=res.text, media_type="application/json", status_code=status.HTTP_200_OK)
